5e922a00da44d0fce846b0ce83e68868c6eec0ad
nicksonlangat <nicksonlangat95@gmail.com>
2023-09-05 17:10:22 +0300

    NV-5-add-delete-button-on-shipments-list-page

--- navis_frontend/src/views/Parcels.vue
+++ navis_frontend/src/views/Parcels.vue
@@ -17,6 +17,7 @@
                     <div>
                         <NewParcelDrawer/>
                         <EditParcelModal/>
+                        <DeleteModal/>
                     </div>
                 </div>
             </div>
@@ -83,9 +84,12 @@
                             <td class="px-6 py-4">
                               {{ parcel.destination.name }}
                             </td>
-                            <td class="px-6 py-4">
-                                <svg @click="editParcel(parcel)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 text-gray-500 cursor-pointer h-4">
+                            <td class="px-6 py-4 flex gap-4">
+                                <svg @click="editParcel(parcel)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 text-gray-500 cursor-pointer hover:text-violet-500 transition-all duration-300 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                </svg>
+                                <svg @click="removeParcel(parcel)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 text-gray-500 cursor-pointer hover:text-red-500 transition-all duration-300 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                                 </svg>
                             </td>
                         </tr>
@@ -104,6 +108,7 @@ import Aside from '@/components/Aside.vue';
 import NewParcelDrawer from '@/components/NewParcelDrawer.vue';
 import EditParcelModal from '@/components/EditParcelModal.vue';
 import EmptyIllustration from '@/components/EmptyIllustration.vue';
+import DeleteModal from '@/components/DeleteModal.vue';
 import {
     mapActions,
     mapGetters
@@ -115,7 +120,8 @@ export default {
         Aside,
         NewParcelDrawer,
         EditParcelModal,
-        EmptyIllustration
+        EmptyIllustration,
+        DeleteModal
     },
     data() {
         return {
@@ -135,6 +141,7 @@ export default {
     methods: {
         ...mapActions({
             getAllParcels: 'getAllParcels',
+            deleteParcel: 'deleteParcel'
         }),
         init() {
             this.getAllParcels({
@@ -146,12 +153,30 @@ export default {
         },
         editParcel(parcel) {
             this.emitter.emit("showParcelModal", parcel)
+        },
+        removeParcel(parcel) {
+            this.emitter.emit("showDeleteModal", {
+                "type": "Parcel",
+                "value": parcel.parcel_number,
+                "id": parcel.id
+            })
+        },
+        deleteConfirmedParcel(id) {
+            this.deleteParcel({
+                uuid: id,
+                cb: (() => {
+                    this.init()
+                })
+            })
         }
     },
     mounted() {
         this.init()
         this.emitter.on("reloadParcels", value => {
             this.init()
+        })
+        this.emitter.on("deleteParcel", id => {
+            this.deleteConfirmedParcel(id)
         })
     }
 }
--- navis_frontend/src/views/Shipments.vue
+++ navis_frontend/src/views/Shipments.vue
@@ -4,6 +4,7 @@
     <div class="w-10/12">
         <div class="pl-6 pr-6 font-base">
             <Search />
+            <DeleteModal/>
             <div class="flex font-base mt-5 justify-between">
                 <div class="flex gap-4 items-center">
                     <h1 class="text-2xl font-extrabold">Shipments</h1>
@@ -51,8 +52,9 @@
                             <th scope="col" class="px-6 py-3">
                                 arrival date
                             </th>
+                           
                             <th scope="col" class="px-6 py-3">
-                                time delay
+                                action
                             </th>
                         </tr>
                     </thead>
@@ -82,8 +84,14 @@
                             <td class="px-6 py-4">
                                 {{ formatDate(shipment.arrival_date) }}
                             </td>
-                            <td class="px-6 py-4">
-                                5:05 h
+                           
+                            <td class="px-6 py-4 flex gap-4">
+                                <svg @click="goToShipmentPage(shipment.id)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 text-gray-500 cursor-pointer hover:text-violet-500 transition-all duration-300 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+                                </svg>
+                                <svg @click="removeShipment(shipment)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 text-gray-500 cursor-pointer hover:text-red-500 transition-all duration-300 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
                             </td>
                         </tr>
 
@@ -135,6 +143,7 @@
 import Aside from '@/components/Aside.vue';
 import Search from '@/components/Search.vue';
 import moment from 'moment';
+import DeleteModal from '@/components/DeleteModal.vue';
 import {
     mapActions,
     mapGetters
@@ -144,6 +153,7 @@ export default {
     components: {
         Aside,
         Search,
+        DeleteModal
     },
     data() {
         return {
@@ -163,7 +173,8 @@ export default {
     },
     methods: {
         ...mapActions({
-            getAllShipments: 'getAllShipments'
+            getAllShipments: 'getAllShipments',
+            deleteShipment: 'deleteShipment'
         }),
         init() {
             this.getAllShipments({
@@ -171,7 +182,7 @@ export default {
             })
         },
         formatDate(date) {
-            return moment(date).format("MMM Do YY")
+            return moment(date).format("DD-MM-YY")
         },
         setShipmentFilter(value) {
             this.currentShimentFilter = value
@@ -181,12 +192,30 @@ export default {
         },
         goToShipmentPage(uuid) {
           this.$router.push({"name": "shipment", "params": {"id": uuid}})
+        },
+        removeShipment(shipment) {
+            this.emitter.emit("showDeleteModal", {
+                "type": "Shipment",
+                "value": shipment.shipment_number,
+                "id": shipment.id
+            })
+        },
+        deleteConfirmedShipment(id) {
+            this.deleteShipment({
+                uuid: id,
+                cb: (() => {
+                    this.init()
+                })
+            })
         }
     },
     mounted() {
         this.init()
         this.emitter.on("reloadShipments", value => {
             this.init()
+        })
+        this.emitter.on("deleteShipment", id => {
+            this.deleteConfirmedShipment(id)
         })
     }
 }
