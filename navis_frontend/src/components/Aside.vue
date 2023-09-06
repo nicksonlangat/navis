2dcdbe37a619a29e694ff33fa64f8533561d0bb5
nicksonlangat <nicksonlangat95@gmail.com>
2023-09-06 10:45:40 +0300

    NV-10-create-login-pages

--- navis_frontend/src/components/Aside.vue
+++ navis_frontend/src/components/Aside.vue
@@ -1,14 +1,14 @@
 <template>
 <div class="bg-white h-screen w-2/12">
-    <div class="pl-4 mt-5 flex gap-3">
-        <img src="../assets//logo.jpg" class="h-16 w-16 rounded-full object-cover" alt="">
-        <div class="mt-2 text-gray-500 font-base">
+    <div class="flex -mt-4 items-center">
+        <img src="../assets//logo.png" class="h-32 w-32 rounded-full object-cover" alt="">
+        <div class="-ml-5 text-gray-500 font-base">
             <h1 class="text-violet-600 text-xl font-bold">Navis</h1>
             <p class="text-xs mt-1"> Workspace</p>
         </div>
     </div>
 
-    <ul class="pl-3 mt-10 flex text-sm flex-col gap-3 text-gray-500 font-base mr-5">
+    <ul class="pl-3 flex text-sm flex-col gap-3 text-gray-500 font-base mr-5">
         <li :class="currentRoute === 'dashboard' ? 'bg-[#F1F1FB] text-gray-900' : ''" @click="navigateToPage('dashboard')" class="flex w-full rounded-md px-4 gap-2 hover:bg-[#F1F1FB] group hover:text-gray-900 cursor-pointer transition-all duration-500 py-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" :class="currentRoute === 'dashboard' ? 'text-violet-500' : ''" class="w-5 group-hover:text-violet-500 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
@@ -29,6 +29,12 @@
             Parcels
             <span class="bg-red-500 text-white h-5 w-5 text-center ml-5 rounded">2</span>
         </li>
+        <li :class="currentRoute === 'clients' ? 'bg-[#F1F1FB] text-gray-900' : ''" @click="navigateToPage('clients')" class="flex w-full rounded-md px-4 gap-2 hover:bg-[#F1F1FB] group hover:text-gray-900 cursor-pointer transition-all duration-500 py-2">
+            <svg :class="currentRoute === 'clients' ? 'text-violet-500' : ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 group-hover:text-violet-500 ">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+            </svg>
+
+            Clients</li>
         <li :class="currentRoute === 'drivers' ? 'bg-[#F1F1FB] text-gray-900' : ''" @click="navigateToPage('drivers')" class="flex w-full rounded-md px-4 gap-2 hover:bg-[#F1F1FB] group hover:text-gray-900 cursor-pointer transition-all duration-500 py-2">
             <svg :class="currentRoute === 'drivers' ? 'text-violet-500' : ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 group-hover:text-violet-500 ">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
@@ -42,15 +48,10 @@
             </svg>
 
             Trucks</li>
-        <li :class="currentRoute === 'clients' ? 'bg-[#F1F1FB] text-gray-900' : ''" @click="navigateToPage('clients')" class="flex w-full rounded-md px-4 gap-2 hover:bg-[#F1F1FB] group hover:text-gray-900 cursor-pointer transition-all duration-500 py-2">
-            <svg :class="currentRoute === 'clients' ? 'text-violet-500' : ''" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 group-hover:text-violet-500 ">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-            </svg>
-
-            Clients</li>
+       
     </ul>
-    <hr class="mt-10">
-    <ul class="pl-3 mt-10 flex text-sm flex-col gap-3 mr-5 text-gray-500 font-base">
+    <hr class="mt-5">
+    <ul class="pl-3 mt-5 flex text-sm flex-col gap-3 mr-5 text-gray-500 font-base">
         <li :class="currentRoute === 'staff' ? 'bg-[#F1F1FB] text-gray-900' : ''" class="flex w-full items-center rounded-md px-4 gap-2 hover:bg-[#F1F1FB] group hover:text-gray-900 cursor-pointer transition-all duration-500 py-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
--- navis_frontend/src/router/index.js
+++ navis_frontend/src/router/index.js
@@ -36,6 +36,11 @@ const routes = [
     path: '/drivers',
     name: 'drivers',
     component: () => import( '../views/Drivers.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import( '../views/Login.vue')
   }
 ]
 
