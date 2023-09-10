4e5a87848e6ef0af59179956a6e73ec90ed704c4
nicksonlangat <nicksonlangat95@gmail.com>
2023-09-10 09:32:37 +0300

    NV-16-add-project-read-me

--- README.md
+++ README.md
@@ -1,19 +1,121 @@
-# Navis
+Navis is a company that provides logistics, shipping, warehousing and fulfilment services. This platform aims to give staff, managers and admins a positive and high experience when using the dashboard. 
 
-## Project setup
-```
-npm install
-```
+HOW IT WORKS:
+Let’s assume we have a customer named Alice. Alice lives in Nairobi county. She has bought a 65 inch Samsung television and a Sony home theatre system. She would like to send these electrical appliances to her parents back at home in Kericho county. That is where Navis comes in. Alice visits Navis' Nairobi office where a Navis staff member welcomes her and registers Alice as a client in the system. Her electrical appliances are recorded in the parcels module as a parcel. The system sends a payment request to her mobile phone and she accepts the request by entering her MOMO PIN. Her parcel is then added to a shipment record that does the Nairobi-Kericho route. Alice leaves Navis’s office. Every time her parcel status changes she receives an email and SMS notification and when the parcel arrives to Kericho, her parents will receive a text message and a phone call to pick up the parcel. At this point the circle is complete. This process is true to all other clients and routes supported by the system.
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+ 
 
-### Compiles and minifies for production
-```
-npm run build
-```
+PLATFORM MODULES:
+The Navis platform includes several modules listed below:
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Shipments module
+
+Parcels module
+
+Drivers module
+
+Trucks module
+
+Clients module
+
+Staff module
+
+Admin dashboard module
+
+Authentication module
+
+Notifications module
+
+Account settings module
+
+Each module has an equivalent backend API and it is described below in detail including how it looks like in the browser:
+
+Authentication module
+The auth module allows the staff of the company to register and log in to the platform so that they can access and operate on the several modules the platform offers. On the backend the User table is role based and therefore logged in staff members are only able to execute tasks that are assigned to their roles. For examples only managers can delete entries while regular staff members can only create, view and update certain modules. The pages look like below:
+
+Login page:
+
+ 
+
+Password reset pages:
+
+
+
+ 
+
+The managers are responsible for registering and verifying the staff. Managers are added to the system by super admins who have 360 access to the system. Upon a successful login request, the user is taken to the dashboard page.
+
+ 
+
+Dashboard page:
+The dashboard page provides essential information in a summarised manner and designed to assist staff in managing the various aspects of each module and allows for a quick glancing of what is happening where. It presents an overview of key data numerically, latest available and important trends on various modules and also introduces a side navigation bar to facilitate easy navigation throughout the dashboard.  It looks like below:
+
+
+ 
+
+Shipments page:
+The shipments page shows a table that lists all shipments that are ongoing, complete or delayed. At the top it allows for searching of a particular shipment using its shipment number and also provisions a button to create a new shipment. It has two other buttons that allows for changing the view from a table view to card view. A staff member can create a new shipment, view and edit its details and the manager can delete the shipment. It looks like so:
+
+
+
+
+
+ 
+
+Shipment page:
+This page facilitates the the control of parcels/goods that are scheduled for shipment and allows parking of parcels to ensure they reach their intended destinations. Here , staff can add or remove parcels to the truck view the parcel list and finish loading the truck in readiness for shipping. The page looks like below:
+
+ 
+
+Parcels page:
+This page lists all parcels in the system and allows for new parcels/goods to be registered into the system. Staff can create and edit parcels while managers can delete parcels. There is also a search bar that allows for searching using a parcel number. It looks like:
+
+
+
+
+ 
+
+Clients page:
+This page lists all clients in the system and allows for new clients to be registered into the system. Staff can create and edit clients while managers can delete clients. There is also a search bar that allows for searching using a client’s name or identity number. It looks like:
+
+
+
+
+ 
+
+Drivers page: 
+This page lists all drivers in the system and allows for new drivers to be registered into the system. Staff can create and edit drivers while managers can delete drivers. There is also a search bar that allows for searching using a driver’s identity  number. It looks like:
+
+
+
+
+ 
+
+Trucks page:
+This page lists all trucks in the system and allows for new trucks to be registered into the system. Staff can create and edit trucks while managers can delete trucks. There is also a search bar that allows for searching using a registration number. It looks like:
+
+
+
+
+ 
+
+Staff page:
+This page lists all staff in the system and allows for new staff to be registered into the system. Staff need to be verified by managers who can also delete staff. There is also a search bar that allows for searching using a staff’s identity number. It looks like:
+
+
+ 
+
+Accounts page:
+The accounts page allows staff to view and update any personal information. It also allows one to change password among other things. It looks like:
+
+Notifications:
+Notifications are crucial in letting the users know what is the result of their actions. Notifications are platform wide and dispatched every time a new client, truck, driver, parcel, shipment is created, edited or deleted. Below is the notifications module in action:
+
+ 
+
+Mobile responsiveness
+The platform is mobile friendly and responsive and here are some of the screens viewed from a mobile device:
+
+ 
+
+ 
--- navis_frontend/src/views/Shipment.vue
+++ navis_frontend/src/views/Shipment.vue
@@ -8,85 +8,65 @@
                 <h1 class="text-2xl">{{ shipment?.route_from.name }} - {{ shipment?.route_to.name }} <span class="text-xs text-gray-500">{{ formatDate(shipment?.departure_date) }}</span></h1>
             </div>
 
-            <div class="grid font-base mt-8 grid-cols-2 gap-y-4 gap-4">
-                <div class="bg-white rounded-md pb-5">
+            <div class="grid font-base mt-3 grid-cols-2 gap-y-4 gap-4">
+                <div class="bg-white rounded-md pb-2">
                     <div class="flex font-base justify-between ml-5 mr-5 mt-3">
                         <h3 class="text-xl">Truck load</h3>
-                        <h2 class="text-yellow-600 text-2xl">{{percentageCalculator(0, shipment?.truck.carry_weight) }} %</h2>
+                        <h2 class="text-violet-600 text-2xl">{{percentageCalculator(shipment?.loaded_kg, shipment?.truck.carry_weight) }} %</h2>
                     </div>
                     <div class="flex justify-between ml-5 mr-5 mt-5 text-lg">
                         <ul class="text-gray-500 flex font-base flex-col gap-5">
                             <li>
                                 <p>Available,kg</p>
-                                <p class="text-gray-900">2000/{{ shipment?.truck.carry_weight }}</p>
+                                <p class="text-gray-900">{{shipment?.loaded_kg}} / {{ shipment?.truck.carry_weight }}</p>
                             </li>
 
                         </ul>
                         <img class="w-3/4" src="../assets/van.svg" alt="">
                     </div>
-                    <div class="flex gap-2 ml-5 mr-5 mt-5 items-center text-gray-700 font-base">
-                        <h3>Upper tier</h3>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-400 ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                    </div>
-                    <div class="grid grid-cols-3 mt-2 ml-5 mr-5 gap-5">
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
+                    <div class="relative mt-3 font-base overflow-x-auto">
+                        <h3 class="ml-6 font-extrabold underline">Parcels in this shipment</h3>
+                        <table class="w-full mt-3 text-xs text-left text-gray-500 font-base accent-violet-600">
+                            <thead class="text-xs text-gray-700 uppercase bg-white border-b">
+                                <tr>
+                                    
+                                    <th scope="col" class="px-6 py-3">
+                                        Parcel number
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Weight
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        destination
+                                    </th>
 
-                        </div>
-                        <div class="bg-emerald-200 h-20 rounded-md">
-
-                        </div>
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
-
-                        </div>
-
-                    </div>
-                    <div class="flex gap-2 ml-5 mr-5 mt-5 items-center text-gray-700 font-base">
-                        <h3>Middle tier</h3>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-400 ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                    </div>
-                    <div class="grid grid-cols-3 mt-2 ml-5 mr-5 gap-5">
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
-
-                        </div>
-                        <div class="bg-emerald-200 h-20 rounded-md">
-
-                        </div>
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
-
-                        </div>
-
-                    </div>
-                    <div class="flex gap-2 ml-5 mr-5 mt-5 items-center text-gray-700 font-base">
-                        <h3>Lower tier</h3>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-400 ">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-                        </svg>
-                    </div>
-                    <div class="grid grid-cols-3 mt-2 ml-5 mr-5 gap-5">
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
-
-                        </div>
-                        <div class="bg-emerald-200 h-20 rounded-md">
-
-                        </div>
-                        <div class="bg-[#f1f1fb] h-20 rounded-md">
-
-                        </div>
-
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="parcel in shipment?.parcels" class="bg-white border-b font-base">
+                                    
+                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                                        {{ parcel?.parcel_number }}
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        {{ parcel?.weight }}
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        {{ parcel.destination?.name }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div class="flex justify-between mr-5 mt-5 ml-5 text-sm font-base">
-                        <button class="bg-[#f1f1fb] py-1.5 px-4 rounded-md text-violet-600">View parcels list</button>
-                        <button class="bg-[#f1f1fb] py-1.5 px-4 rounded-md text-violet-600">Finish loading</button>
+                        <button @click="printParcels" class="bg-violet-600 text-white py-1.5 px-4 rounded-md text-violet-600">Print parcels list</button>
+                        <button @click="finishLoading" class="bg-violet-600 text-white py-1.5 px-4 rounded-md text-violet-600">Finish loading</button>
                     </div>
                 </div>
                 <div class="bg-white rounded-md pb-5">
                     <div class="flex items-center justify-between font-base ml-3 mt-2 mr-3">
                         <h3 class="font-bold">Available packages</h3>
-                        <button @click.prevent="updateShipmentDetails" class="bg-violet-600 text-xs py-1 px-4 rounded-md text-white">Add selections to truck</button>
+                        <button @click.prevent="updateShipmentDetails" class="bg-violet-600 text-sm py-1 px-4 rounded-md text-white">Add selections to truck</button>
                     </div>
                     <div class="relative mt-2 font-base overflow-x-auto">
                         <table class="w-full text-xs text-left text-gray-500 font-base accent-violet-600">
@@ -227,6 +207,24 @@ export default {
                 })
             })
         },
+        printParcels() {
+
+        },
+        finishLoading() {
+            this.shipment["status"] = "ON WAY"
+            this.shipment.truck = this.shipment.truck.id
+            this.shipment.driver = this.shipment.driver.id
+            this.shipment.route_from = this.shipment.route_from.id
+            this.shipment.route_to = this.shipment.route_to.id
+            this.shipment.parcels = this.shipmentParcels
+            this.updateShipment({
+                uuid: this.shipment.id,
+                payload: this.shipment,
+                cb: (() => {
+                    this.$router.push({"name": "shipments"})
+                })
+            })
+        }
     },
     mounted() {
         this.shipmentId = this.$route.params['id']
