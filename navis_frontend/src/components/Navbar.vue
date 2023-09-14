1b5d437596f2827acbba7063d3dd5f5fe8e8763c
nicksonlangat <nicksonlangat95@gmail.com>
2023-09-14 13:53:00 +0300

    NV-22-make-the-platform-responsive

--- navis_frontend/src/components/Navbar.vue
+++ navis_frontend/src/components/Navbar.vue
@@ -3,10 +3,15 @@
     <div class="flex items-center">
         <h1 class="text-violet-600 text-4xl font-bold">Navis</h1>
     </div>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
+    
+    <Mobile/>
 </div>
 </template>
 <script>
+import Mobile from './Mobile.vue';
+export default {
+    components: {
+        Mobile
+    }
+}
 </script>
