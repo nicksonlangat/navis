f990e3b4e1ef88ca3b43253be4f40a4bd227239e
nicksonlangat <nicksonlangat95@gmail.com>
2023-09-14 16:32:47 +0300

    git checkout -b NV-32-initial-deployment

--- navis_backend/mysite/settings.py
+++ navis_backend/mysite/settings.py
@@ -125,10 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800 
